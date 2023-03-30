2ea532cbe5357587d4f8010b5b84556ca419d965
Alper Karagöz <alperkaragoz@outlook.com>
2023-03-30 23:30:53 +0300

    min changes

--- AspNetCoreRateLimitExam/Program.cs
+++ AspNetCoreRateLimitExam/Program.cs
@@ -6,7 +6,6 @@ var provider = builder.Services.BuildServiceProvider();
 var configuration = provider.GetRequiredService<IConfiguration>();
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
